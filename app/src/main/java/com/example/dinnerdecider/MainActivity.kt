c075ee0bfea9152b430a9c18387401c232e07360
adam <adam.boyer@rocketmail.com>
2020-10-28 16:59:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.dinnerdecider
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.content_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodlist = arrayListOf("Chinese","Hamburger","McD's","Wendys","Cat Food")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
+        decideButton.setOnClickListener {
+            //println("You clicked me")
+            val random = Random()
+            val randomfood = random.nextInt(foodlist.count())
+            SelectedFoodText.text = foodlist[randomfood]
+        }
+
+        addFoodButton.setOnClickListener {
+            val newfood = addFoodText.text.toString()
+            foodlist.add(newfood)
+            addFoodText.text.clear()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
